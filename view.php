868b6553daad5d3e7015a37ee57395219d097c1b
Shivalokeshp <47849052+Shivalokeshp@users.noreply.github.com>
2020-10-29 15:33:45 +0530

    Create view.php

---
+++ view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	
+	$fp = fopen("counterlog.txt", "r");
+	$count = fread($fp, 10);
+	fclose($fp);
+	$count = $count + 1;
+	echo "<p>page views:" . $count . "</p>";
+	$fp = fopen("counterlog.txt", "w");
+	fwrite($fp, $count);
+	fclose($fp);
+
+?>
+</body>
+</html>
